3840a7e3b519bb03d02bbbb20011878ed590baf9
Abhijit Roy <dalvath3700@gmail.com>
2020-07-06 02:12:56 +0530

    hodl contract created for 'payable actions'

---
+++ base/hodl/README.md
@@ -0,0 +1,31 @@
+# `hodl` contract
+## Account details:
+* Network: "Jungle Testnet"
+* Contract account: "bhub1111hodl"
+* Contract name: "hodl"
+* [hodl.cpp](./hodl.cpp)
+* [hodl.wasm](./hodl.wasm)
+* [hodl.abi](./hodl.abi)
+
+## About
+As for the logic of this smart contract, we're going to write a contract that accepts a particular token but will not allow the tokens to be withdrawn for a specific amount of time.
+
+This contract needs to set up a few constraints:
+
+* What symbol/token does this contract accept?
+	- `"SYS"` token
+* When is the hodl over?
+	- `the_party` constant sets the hodl to end on `Tuesday, February 22, 2022 10:22:22 PM`.
+
+## RAM Table
+* The contract table is definitely stored in the contract account.
+* `ram_payer`: "bhub1111hodl"
+* `scope`: hodler i.e. whoever the holds the money.
+
+## Compile
+
+## Deploy
+
+## Push
+
+## Table
---
+++ base/hodl/hodl.cpp
@@ -0,0 +1,61 @@
+#include <eosio/eosio.hpp>
+
+using eosio::contract;
+using eosio::check;
+using std::string;
+using eosio::symbol;
+using eosio::multi_index;
+using eosio::asset;
+using eosio::same_payer;
+
+class [[eosio::contract]] hodl : public contract
+{
+private:
+	static const uint32_t the_party = 1645568542;	// UTC timestamp for "Tuesday, February 22, 2022 10:22:22 PM"
+	const symbol hodl_symbol;
+public:
+	using contract::contract;
+
+	hodl(name receiver, name code, datastream<const char*> ds) : contract(code, receiver, ds), hodl_symbol("SYS", 4) {}
+
+	ACTION hodl(const name& hodler, const name& to, const asset& quantity, const string& memo) {
+		if(hodler == get_self() || to != get_self()) {
+			print("These are not the droids you are looking for.");
+			return;
+		}
+
+		// check for the conditions
+		check(now() < the_party, "you are way late");
+		check(quantity.amount > 0, "when pigs fly!");
+		check(quantity.symbol() == hodl_symbol, "These are not the droids you are looking for.");
+
+		hodl_index hodl_table(get_self(), hodler.value);
+		auto hodl_it = hodl_table.find(hodler.value);
+
+		if(hodl_it == hodl_table.end()) {
+			hodl_table.emplace(hodler, [&](auto& row) {
+				row.funds = quantity;
+			});
+		} else {
+			hodl_table.modify(hodl_it, same_payer, [&](auto& row) {
+				row.funds += quantity;
+			});
+		}
+
+
+	}
+
+	inline uint32_t now() const {
+		return current_time_point().sec_since_epoch();
+	}
+
+private:
+	TABLE hodl
+	{
+		asset funds;
+
+		auto primary_key() const { return funds.symbol.raw(); }
+	};
+
+	using hodl_index = multi_index<"hodl"_n, hodl>;
+};
