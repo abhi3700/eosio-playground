cc712a0ad4b0091cb1847c3b2a55109c823b1cd8
Abhijit Roy <dalvath3700@gmail.com>
2020-08-28 01:37:18 +0530

    tabletest2 contract added & tested successfully

--- base/tabletest2/tabletest2.cpp
+++ base/tabletest2/tabletest2.cpp
@@ -39,63 +39,59 @@ public:
 			ridetable.emplace(get_self(), [&](auto& row) {
 				row.commuter = commuter;
 				row.ride_action = ride_action;
-				// row.txn_vector.emplace_back(make_pair(ride_action.to_string(), get_trxid_str()));
-				creatify_vector_pair(row.txn_vector, ride_action.to_string(), get_trxid());
+				creatify_vector_pair(row.txn_vector, ride_action.to_string(), get_trxid_str());
 
 			});
 		} else {
 			ridetable.modify(ridetable_it, get_self(), [&](auto& row) {
 				row.ride_action = ride_action;
-				// row.txn_vector.emplace_back(make_pair(ride_action.to_string(), get_trxid_str()));
-				creatify_vector_pair(row.txn_vector, ride_action.to_string(), get_trxid());
+				creatify_vector_pair(row.txn_vector, ride_action.to_string(), get_trxid_str());
 
 			});
 		}
 	}
 
-	ACTION del(const name& commuter) {
-		require_auth(get_self());
-
-		ridetable_index ridetable(get_self(), get_self().value);
-		auto ridetable_it = ridetable.find(commuter.value);
-
-		check(ridetable_it != ridetable.end(), "Commuter info exists in the table.");
-		ridetable.erase(ridetable_it);
-
-	}
-
-
 	// NOTE: vector arg can't be const as emplace_back is non-const method
-	inline void creatify_vector_pair( vector<pair<string, checksum256>>& v, const string& s, const checksum256& val ) {
+	inline void creatify_vector_pair( vector<pair_t>& v, const string& s, const string& val ) {
 		auto s_it = std::find_if(v.begin(), v.end(), [&](auto& vs){ return vs.first == s; });
 		if(s_it != v.end()) {		// key found
-			s_it->second = val;
+			s_it->second.emplace_back(val);
 		}
 		else {						// key NOT found
-			v.emplace_back(make_pair(s, val));
+			v.emplace_back(make_pair(s, vector<string>{val}));
 		}
 	}
-	
+
+
 private:
 	TABLE ridetable {
 		name commuter;
 		name ride_action;
-		// vector<pair_t> txn_vector;
-		vector<pair<string, checksum256>> txn_vector;
+		vector<pair_t> txn_vector;
 
 		auto primary_key() const { return commuter.value; }
 	};
 
 	using ridetable_index = multi_index<"ridetable"_n, ridetable>;
 	// ----------------------------------------------------------------------------------
-	inline checksum256 get_trxid()
-	{
-	  auto trxsize = transaction_size();
-	  char trxbuf[trxsize];
-	  uint32_t trxread = read_transaction( trxbuf, trxsize );
-	  check( trxsize == trxread, "read_transaction failed");
-	  return sha256(trxbuf, trxsize);
+	inline string get_trxid_str() {
+		auto trxsize = transaction_size();
+		char trxbuf[trxsize];
+		uint32_t trxread = read_transaction( trxbuf, trxsize );
+		check( trxsize == trxread, "read_transaction failed");
+		auto hash_digest = sha256(trxbuf, trxsize);
+		return hex_to_str(&hash_digest, sizeof(hash_digest));
 	}
 
+	template<typename CharT>
+	static string hex_to_str(const CharT* d, uint32_t s) {
+	  std::string r;
+	  const char* to_hex="0123456789abcdef";
+	  uint8_t* c = (uint8_t*)d;
+	  for( uint32_t i = 0; i < s; ++i ) {
+	    (r += to_hex[(c[i] >> 4)]) += to_hex[(c[i] & 0x0f)];
+	  }
+	  return r;
+	}
 
 };
