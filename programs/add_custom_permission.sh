760ede69fdabaf02f05b832c5bf15cbe77f215db
Abhijit Roy <dalvath3700@gmail.com>
2021-01-28 12:22:35 +0530

    programs folder created

---
+++ programs/add_custom_permission.sh
@@ -0,0 +1,3 @@
+# Add a custom permission for an ACTION of a contract
+# this is for security reason just like in play, disndcards ACTION of WAX.
+# The key is used inside the FE-code which if compromised then the account is secure, as neither the active, owner is compromised.
