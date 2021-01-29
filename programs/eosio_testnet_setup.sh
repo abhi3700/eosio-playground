0f736ae353b437cfe6bd8271a0ea60bb1560621f
Abhijit Roy <dalvath3700@gmail.com>
2021-01-29 22:18:18 +0530

    EOSIO testnet setup updated

--- programs/eosio_testnet_setup.sh
+++ programs/eosio_testnet_setup.sh
@@ -1,10 +1,26 @@
+# =================================================================
+# Setup your local DApp development environment sin CLI
+# =================================================================
 # open bash file
 nano ~/.bashrc
-# calling cleos -u .... as cleost for EOS testnet
+
+# -----------------------------------------------------------------
+# Refer RPI API for different networks 
+# source: https://github.com/abhi3700/My_Learning_EOS/blob/master/README.md
+# EOS Mainnet
+alias cleosm='cleos -u https://eos.greymass.com'
+
+# EOS Testnet | Jungle3                                                                                                                                                     
 alias cleost='cleos -u http://jungle3.cryptolions.io:80'
 
+# WAX Mainnet
+alias cleoswm='cleos -u https://chain.wax.io/'                                                                                                                              
+
+# WAX Testnet
+alias cleoswt='cleos -u https://api.waxtest.greeneosio.com/'
+# -----------------------------------------------------------------
 # call source by executing
 source ~/.bashrc
 
 # check wax network
-cleost get info
+cleoswt get info
