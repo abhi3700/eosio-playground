a58d0c07659acbadce693a2bfcc4812d51a96670
abhi3700 <dalvath3700@gmail.com>
2021-04-18 23:07:33 +0530

    buysellram, new_acc shell program added

---
+++ programs/buysell_ram.sh
@@ -0,0 +1,2 @@
+# cleos system buyram <payer_ac> <to_ac> <amount> -p <payer_ac>@<permission>
+cleos system buyram gpkbattlesco gbuser111112 "0.1 EOS" -p gpkbattlesco@active
---
+++ programs/new_account.sh
@@ -0,0 +1,1 @@
+cleosw system newaccount gpkbattlesco gbuser111112 EOS7r6ZWR4wCD1biPGekaKkju5NjEKmMX3U49ccmZScmZKHRtbTfa EOS7r6ZWR4wCD1biPGekaKkju5NjEKmMX3U49ccmZScmZKHRtbTfa --stake-net "0.2 WAX" --stake-cpu "0.2 WAX" --buy-ram-kbytes 5
