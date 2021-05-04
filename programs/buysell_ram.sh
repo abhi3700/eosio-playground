9aa32af9e4e927b98f591b8689ce88a74c97a04e
Abhijit Roy <dalvath3700@gmail.com>
2021-05-04 22:01:50 +0530

    buysell_ram modified how to buy in bytes, kbytes

--- programs/buysell_ram.sh
+++ programs/buysell_ram.sh
@@ -1,2 +1,9 @@
-# cleos system buyram <payer_ac> <to_ac> <amount> -p <payer_ac>@<permission>
+# permission is payer_ac@active by default.
+# cleos system buyram <payer_ac> <to_ac> <amount_in_EOS> -p <payer_ac>@<permission>
 cleos system buyram gpkbattlesco gbuser111112 "0.1 EOS" -p gpkbattlesco@active
+
+# cleos system buyram <payer_ac> <to_ac> -b <amount_in_bytes> -p <payer_ac>@<permission>
+cleos system buyram hello1111111 hello1111111 -b 100 -p hello1111111@active
+
+# cleos system buyram <payer_ac> <to_ac> -k <amount_in_kbytes> -p <payer_ac>@<permission>
+cleos system buyram hello1111111 hello1111111 -k 10 -p hello1111111@active
